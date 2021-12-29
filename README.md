8e37660011b98849fbca138a1f04660f39b60c6c
Luiz Schlischting <luiz.schlischting@dotgroup.com.br>
2021-12-29 14:22:15 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# simple proxy + adminer + mariadb + php56
+
+## work path 
+  * /var/www/localhost
+
+## Run
+  * docker-compose up
---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+$servername = "db.local";
+$username = "root";
+$password = "mysqlpass";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
